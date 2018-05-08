f3a81254a108306dec7b743e3c8dd9b8c17d2111
lhy757208 <757208466@qq.com>
2018-05-08 10:55:40 +0800

    222

--- application/api/service/Order.php
+++ application/api/service/Order.php
@@ -24,6 +24,22 @@ class Order
         $this->oProducts = $oProducts;
         //获取订单中商品的原始信息
         $this->products = $this->getProductsByOrders($oProducts);
+
+        //比较订单中的库存
+        $status = $this->getOrderStatus();
+        return json($status);
+    }
+
+    protected function getOrderStatus()
+    {
+        //因为买的这个商品不是一个
+        $status = [
+            'pass'=>'true',
+            'orderPrice'=>0,
+            'pStatusArray'=>[]
+        ];
+
+        
     }
 
     protected function getProductsByOrders($oProducts)
